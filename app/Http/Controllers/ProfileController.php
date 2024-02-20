d44127cc86f41d368a1086b215298228bab1b037
gvieiragoulart <vieirabiel2009@hotmail.com>
2024-02-20 17:19:07 -0300

    commit teste

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,7 +14,7 @@ use Inertia\Response;
 class ProfileController extends Controller
 {
     /**
-     * Display the user's profile form.
+     * Display the user''s profile form.
      */
     public function edit(Request $request): Response
     {
