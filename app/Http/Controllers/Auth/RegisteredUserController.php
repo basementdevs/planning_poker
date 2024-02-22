c4386464c1cbe5614de9849198f681cfe7974fdb
Lucas Pace <lucas.pace@hotmail.com.br>
2024-02-22 19:03:50 -0300

    feat: users can be guest

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,6 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-
         return redirect(RouteServiceProvider::HOME);
     }
 }
