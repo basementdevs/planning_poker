c03a088df6e91ed7730f172d392f11b62a406bd9
petcha <petcha@pop-os.localdomain>
2024-02-25 20:41:57 -0300

    feat/partial: added some migrations  and models

---
+++ database/factories/SettingFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Setting;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SettingFactory extends Factory
+{
+    protected $model = Setting::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
